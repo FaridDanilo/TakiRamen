ed2d0695c7099a5ab872b08eeed15e4b42309be1
Danilo <fdgrijalbac@gmail.com>
2025-10-30 18:40:54 -0500

    Takiramen FrontEnd V1

--- assets/scripts.js
+++ assets/scripts.js
@@ -104,9 +104,3 @@ startAutoPlay();
 track.parentElement.addEventListener('mouseenter', stopAutoPlay);
 track.parentElement.addEventListener('mouseleave', startAutoPlay);
 //* Carrusel The Best Start
-
-document.addEventListener('keydown', function(e) {
-    if (e.ctrlKey && e.altKey && e.key === 'a') {
-        document.querySelector('.form-container').classList.toggle('show');
-    }
-});
--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/Projects/takiramen/');
-
-    define('ASSETS_URL', BASE_URL . 'assets/');
-
-?>
